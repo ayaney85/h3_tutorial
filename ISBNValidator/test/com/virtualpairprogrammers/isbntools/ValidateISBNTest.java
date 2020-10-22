e84620be44a270b7ffb7980d54bc2336610d0010
ayaney85 <osma8251@stthomas.edu>
2020-10-22 02:22:59 -0500

    This is a failing test of the checkaninvalidISBN

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -14,5 +14,11 @@ public class ValidateISBNTest
 		boolean result = validator.checkISBN(140449116);
 		assertTrue(result);
 	}
-
+	@Test
+	public void checkAnInvalidISBN()
+	{
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449117);
+		assertFalse(result);
+	}
 }
