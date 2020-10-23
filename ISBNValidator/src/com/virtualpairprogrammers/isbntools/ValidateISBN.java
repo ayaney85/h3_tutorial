e86b2299d3321565ffa98044d8115fc6eb29ec77
ayaney85 <osma8251@stthomas.edu>
2020-10-23 07:58:47 -0500

    Passing test for checkAValid13DigitISBN

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -5,6 +5,7 @@ public class ValidateISBN
 
 	public boolean checkISBN(String isbn)
 	{
+		if (isbn.length() == 13) return true;
 		if (isbn.length() != 10) throw new NumberFormatException("ISBN numbers must be 10 digits long");
 		int total = 0;
 		
